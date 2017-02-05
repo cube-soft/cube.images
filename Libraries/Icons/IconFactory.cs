ec88f0925634f2b1ca505e138f1aca3129637cd8
clown <tt.clown@gmail.com>
2017-02-06 00:12:54 +0900

    Cube.Images 1.3.x へ向けた修正

---
+++ Libraries/Icons/IconFactory.cs
@@ -0,0 +1,158 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// IconFactory.cs
+/// 
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+using System.Drawing;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Cube.Images.Icons
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// IconFactory
+    /// 
+    /// <summary>
+    /// Icon を生成するためのクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    public static class IconFactory
+    {
+        #region Methods
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Create
+        ///
+        /// <summary>
+        /// システムで用意されているアイコンを生成します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static Icon Create(StockIcons id, IconSize size)
+        {
+            var info = new SHSTOCKICONINFO();
+            info.cbSize = Marshal.SizeOf(info);
+            Shell32.NativeMethods.SHGetStockIconInfo((uint)id,
+                Shell32.NativeMethods.SHGFI_SYSICONINDEX, ref info);
+
+            IImageList images;
+            Shell32.NativeMethods.SHGetImageList((uint)size,
+                Shell32.NativeMethods.IID_IImageList, out images);
+            if (images == null) return null;
+
+            var handle = IntPtr.Zero;
+            images.GetIcon(info.iSysImageIndex,
+                (int)Shell32.NativeMethods.ILD_TRANSPARENT, ref handle);
+            return (handle != IntPtr.Zero) ? Icon.FromHandle(handle) : null;
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Create
+        ///
+        /// <summary>
+        /// 指定されたファイルからアイコンを生成します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static Icon Create(System.IO.FileInfo fi, IconSize size)
+            => Create(fi?.FullName, size);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Create
+        ///
+        /// <summary>
+        /// 指定されたファイルからアイコンを生成します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static Icon Create(string path, IconSize size)
+        {
+            var info = new SHFILEINFO();
+            Shell32.NativeMethods.SHGetFileInfo(path, 0, ref info, (uint)Marshal.SizeOf(info),
+                Shell32.NativeMethods.SHGFI_SYSICONINDEX);
+
+            IImageList images;
+            Shell32.NativeMethods.SHGetImageList((uint)size,
+                Shell32.NativeMethods.IID_IImageList, out images);
+            if (images == null) return null;
+
+            var handle = IntPtr.Zero;
+            images.GetIcon(info.iIcon, (int)Shell32.NativeMethods.ILD_TRANSPARENT, ref handle);
+            return (handle != IntPtr.Zero) ? Icon.FromHandle(handle) : null;
+        }
+
+        #endregion
+
+        #region Extensions
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetIcon
+        ///
+        /// <summary>
+        /// Assembly オブジェクトに対応するアイコンを取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static Icon GetIcon(this Assembly asm, IconSize size)
+            => Create(asm?.Location, size);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetIcon
+        ///
+        /// <summary>
+        /// Assembly オブジェクトに対応するアイコンを取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static Icon GetIcon(this AssemblyReader reader, IconSize size)
+            => GetIcon(reader?.Assembly, size);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetIcon
+        ///
+        /// <summary>
+        /// システムで用意されているアイコンを取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static Icon GetIcon(StockIcons si, IconSize size)
+            => Create(si, size);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// GetIcon
+        ///
+        /// <summary>
+        /// 指定されたファイルからアイコンを取得します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public static Icon GetIcon(System.IO.FileInfo fi, IconSize size)
+            => Create(fi?.FullName, size);
+
+        #endregion
+    }
+}
---
+++ Libraries/NativeMethods/Shell32.cs
@@ -0,0 +1,80 @@
+﻿/* ------------------------------------------------------------------------- */
+///
+/// Shell32.cs
+/// 
+/// Copyright (c) 2010 CubeSoft, Inc.
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///  http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/* ------------------------------------------------------------------------- */
+using System;
+using System.Runtime.InteropServices;
+
+namespace Cube.Images.Shell32
+{
+    /* --------------------------------------------------------------------- */
+    ///
+    /// Shell32
+    /// 
+    /// <summary>
+    /// shell32.dll に定義された関数を宣言するためのクラスです。
+    /// </summary>
+    ///
+    /* --------------------------------------------------------------------- */
+    internal static class NativeMethods
+    {
+        /* ----------------------------------------------------------------- */
+        ///
+        /// SHGetFileInfo
+        ///
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/bb762179.aspx
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [DllImport(LibName, CharSet = CharSet.Unicode)]
+        public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes,
+            ref SHFILEINFO psfi, uint cbFileInfo, uint uFlags);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// SHGetStockIconInfo
+        ///
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/bb762205.aspx
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [DllImport(LibName, CharSet = CharSet.Unicode)]
+        public static extern void SHGetStockIconInfo(uint siid, uint uFlags, ref SHSTOCKICONINFO sii);
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// SHGetImageList
+        ///
+        /// <summary>
+        /// https://msdn.microsoft.com/en-us/library/windows/desktop/bb762185.aspx
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [DllImport(LibName, CharSet = CharSet.Unicode)]
+        public static extern int SHGetImageList(uint iImageList, [MarshalAs(UnmanagedType.LPStruct)] Guid riid, out IImageList ppv);
+
+        #region Constant fields
+        private const string LibName = "shell32.dll";
+        public const uint SHGFI_SYSICONINDEX = 0x00004000;
+        public const uint ILD_TRANSPARENT = 0x00000001;
+        public static readonly Guid IID_IImageList = new Guid("46EB5926-582E-4017-9FDF-E8998DAA0950");
+        #endregion
+    }
+}
