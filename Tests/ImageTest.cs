b131966b9548b07c09c0812dac6d75dca147c67b
clown <tt.clown@gmail.com>
2017-02-10 14:48:51 +0900

    細かな修正

--- Tests/ImageTest.cs
+++ Tests/ImageTest.cs
@@ -1,7 +1,5 @@
 ﻿/* ------------------------------------------------------------------------- */
 ///
-/// ImageResizer.cs
-/// 
 /// Copyright (c) 2010 CubeSoft, Inc.
 /// 
 /// Licensed under the Apache License, Version 2.0 (the "License");
