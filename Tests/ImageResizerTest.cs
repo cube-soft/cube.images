a894192e179c1a78e39f8b2a38e3c3a3da98e014
clown <tt.clown@gmail.com>
2017-05-20 16:44:02 +0900

    Update tests

--- Tests/ImageResizerTest.cs
+++ Tests/ImageResizerTest.cs
@@ -15,7 +15,9 @@
 /// limitations under the License.
 ///
 /* ------------------------------------------------------------------------- */
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using NUnit.Framework;
 using IoEx = System.IO;
 
@@ -51,13 +53,13 @@ namespace Cube.Images.Tests
         public int Resized_Height(int width, bool preserve, bool shrink)
         {
             var filename = "lena.png";
-            using (var resizer = Create(filename))
+            using (var resizer = new ImageResizer(Example(filename)))
             {
                 var ext = IoEx.Path.GetExtension(filename);
                 resizer.PreserveAspectRatio = preserve;
                 resizer.ShrinkOnly = shrink;
                 resizer.Width = width;
-                resizer.Save(CreateFilePath(resizer, "resized", ext));
+                resizer.Save(SavePath(resizer, "resized", ext));
 
                 return resizer.Resized.Height;
             }
@@ -72,18 +74,18 @@ namespace Cube.Images.Tests
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        [TestCase("lena.png",       600, 801429L)]
-        [TestCase("lena-24bpp.jpg", 600,  37817L)]
-        [TestCase("lena.png",       512, 801429L)]
-        [TestCase("lena-24bpp.jpg", 512,  37817L)]
-        [TestCase("lena.png",       256, 197792L)]
-        [TestCase("lena-24bpp.jpg", 256,  12540L)]
-        [TestCase("lena.png",       128,  50928L)]
-        [TestCase("lena-24bpp.jpg", 128,   4690L)]
+        [TestCase("lena.png",       600, 801000L)]
+        [TestCase("lena-24bpp.jpg", 600,  37000L)]
+        [TestCase("lena.png",       512, 801000L)]
+        [TestCase("lena-24bpp.jpg", 512,  37000L)]
+        [TestCase("lena.png",       256, 197000L)]
+        [TestCase("lena-24bpp.jpg", 256,  12000L)]
+        [TestCase("lena.png",       128,  50000L)]
+        [TestCase("lena-24bpp.jpg", 128,   4000L)]
         public void Save_Stream(string filename, int width, long expected)
         {
             using (var dest = new System.IO.MemoryStream())
-            using (var resizer = Create(filename))
+            using (var resizer = new ImageResizer(Example(filename)))
             {
                 resizer.ResizeMode = ImageResizeMode.HighQuality;
                 resizer.PreserveAspectRatio = true;
@@ -103,19 +105,67 @@ namespace Cube.Images.Tests
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        [TestCase(100, 256, 59309L)]
-        [TestCase( 75, 256, 12389L)]
-        [TestCase( 50, 256,  8548L)]
-        [TestCase( 25, 256,  5763L)]
+        [TestCase(100, 256, 59000L)]
+        [TestCase( 75, 256, 12000L)]
+        [TestCase( 50, 256,  8000L)]
+        [TestCase( 25, 256,  5000L)]
         public void Save_Stream_Jpeg(long quality, int width, long expected)
         {
             var filename = "lena.png";
             using (var dest = new System.IO.MemoryStream())
-            using (var resizer = Create(filename))
+            using (var resizer = new ImageResizer(Example(filename)))
             {
                 resizer.ResizeMode = ImageResizeMode.HighQuality;
                 resizer.Width = width;
                 resizer.Save(dest, Jpeg.Format, Jpeg.Quality(quality));
+                Assert.That(dest.Length, Is.AtLeast(expected));
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Save_Stream_Png
+        ///
+        /// <summary>
+        /// PNG 形式で Stream に書き出すテストを実行します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [TestCase(512, 653000L)]
+        [TestCase(256, 179000L)]
+        [TestCase(128,  46000L)]
+        public void Save_Stream_Png(int width, long expected)
+        {
+            var filename = "lena-24bpp.jpg";
+            using (var dest = new System.IO.MemoryStream())
+            using (var resizer = new ImageResizer(Example(filename)))
+            {
+                resizer.ResizeMode = ImageResizeMode.HighQuality;
+                resizer.Width = width;
+                resizer.Save(dest, Png.Format);
+                Assert.That(dest.Length, Is.AtLeast(expected));
+            }
+        }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Save_Stream_Bmp
+        ///
+        /// <summary>
+        /// BMP 形式で Stream に書き出すテストを実行します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        [TestCase("lena.png",       60000L)]
+        [TestCase("lena-24bpp.jpg", 40000L)]
+        public void Save_Stream_Bmp(string filename, long expected)
+        {
+            using (var dest = new System.IO.MemoryStream())
+            using (var resizer = new ImageResizer(Example(filename)))
+            {
+                resizer.ResizeMode = ImageResizeMode.HighQuality;
+                resizer.Width = 128;
+                resizer.Save(dest, Bmp.Format);
                 Assert.That(dest.Length, Is.AtLeast(expected));
             }
         }
@@ -135,18 +185,37 @@ namespace Cube.Images.Tests
         public void Save_ResizeMode(ImageResizeMode mode)
         {
             var filename = "lena.png";
-            using (var resizer = Create(filename))
+            using (var resizer = new ImageResizer(Example(filename)))
             {
                 resizer.ResizeMode = mode;
                 resizer.Width = 256;
 
                 var ext  = IoEx.Path.GetExtension(filename);
-                var dest = CreateFilePath(resizer, "mode", ext);
+                var dest = SavePath(resizer, "mode", ext);
                 resizer.Save(dest);
 
                 Assert.That(IoEx.File.Exists(dest));
             }
         }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// Create_Null_Throws
+        ///
+        /// <summary>
+        /// Image オブジェクトが null の場合の挙動を確認します。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        public void Create_Null_Throws()
+            => Assert.That(() =>
+            {
+                using (var resizer = new ImageResizer(default(Bitmap)))
+                {
+                    Assert.Fail("never reached");
+                }
+            },
+            Throws.TypeOf<ArgumentException>());
 
         /* ----------------------------------------------------------------- */
         ///
@@ -161,12 +230,12 @@ namespace Cube.Images.Tests
         [TestCase("lena-24bpp.jpg")]
         public void Overwrite(string filename)
         {
-            using (var resizer = Create(filename))
+            using (var resizer = new ImageResizer(Example(filename)))
             {
                 resizer.Width = 128;
 
                 var ext  = IoEx.Path.GetExtension(filename);
-                var dest = CreateFilePath(resizer, "overwrite", ext);
+                var dest = SavePath(resizer, "overwrite", ext);
                 resizer.Save(dest);
                 resizer.Save(dest); // overwrite
 
@@ -180,38 +249,26 @@ namespace Cube.Images.Tests
 
         /* ----------------------------------------------------------------- */
         ///
-        /// Create
-        ///
-        /// <summary>
-        /// ImageResizer オブジェクトを生成します。
-        /// </summary>
-        ///
-        /* ----------------------------------------------------------------- */
-        private ImageResizer Create(string filename)
-            => new ImageResizer(IoEx.Path.Combine(Examples, filename));
-
-        /* ----------------------------------------------------------------- */
-        ///
-        /// CreateFilePath
+        /// SavePath
         ///
         /// <summary>
         /// 保存用ファイルパスを生成します。
         /// </summary>
         ///
         /* ----------------------------------------------------------------- */
-        private string CreateFilePath(ImageResizer resizer, string prefix, string extension)
+        private string SavePath(ImageResizer resizer, string prefix, string extension)
         {
-            var items = new List<string>();
-
-            items.Add(prefix);
-            items.Add($"w{resizer.Width}");
-            items.Add($"h{resizer.Height}");
-            items.Add(resizer.ResizeMode.ToString().ToLower());
-
+            var items = new List<string>
+            {
+                prefix,
+                $"w{resizer.Width}",
+                $"h{resizer.Height}",
+                resizer.ResizeMode.ToString().ToLower()
+            };
             if (resizer.PreserveAspectRatio) items.Add("preserve");
             if (resizer.ShrinkOnly) items.Add("shrink");
 
-            return IoEx.Path.Combine(Results, $"{string.Join("-", items)}{extension}");
+            return Result($"{string.Join("-", items)}{extension}");
         }
 
         #endregion
