9ebf0f896c4f82e7baa17458dbf8ebd9179523f7
clown <tt.clown@gmail.com>
2018-10-06 15:13:18 +0900

    Fix settings.

--- Libraries/Properties/AssemblyInfo.cs
+++ Libraries/Properties/AssemblyInfo.cs
@@ -2,15 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyVersion("1.12.0.0")]
+[assembly: AssemblyFileVersion("1.12.0.0")]
 [assembly: AssemblyTitle("Cube.Images")]
-[assembly: AssemblyDescription("Image library for CubeSoft applications.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CubeSoft")]
 [assembly: AssemblyProduct("Cube.Images")]
+[assembly: AssemblyDescription("Image library for CubeSoft applications.")]
+[assembly: AssemblyCompany("CubeSoft")]
 [assembly: AssemblyCopyright("Copyright © 2010 CubeSoft, Inc.")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("93009dc4-969e-4354-9b32-770b84817ccc")]
-[assembly: AssemblyVersion("1.12.0.0")]
-[assembly: AssemblyFileVersion("1.12.0.0")]
--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -2,15 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyVersion("1.12.0.0")]
+[assembly: AssemblyFileVersion("1.12.0.0")]
 [assembly: AssemblyTitle("Cube.Images.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CubeSoft")]
 [assembly: AssemblyProduct("Cube.Images.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("CubeSoft")]
 [assembly: AssemblyCopyright("Copyright © 2010 CubeSoft, Inc.")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f1cc96df-9065-48c4-b7a8-fcf8387e6250")]
-[assembly: AssemblyVersion("1.12.0.0")]
-[assembly: AssemblyFileVersion("1.12.0.0")]
